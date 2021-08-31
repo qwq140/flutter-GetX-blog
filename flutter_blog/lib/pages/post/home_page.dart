714b2da6857cdebfaba6d8df0f8b5f8de9c43131
unknown <qwq140@gmail.com>
2021-08-31 16:53:51 +0900

    6. 블로그 - HomePage 완성(ListView)

--- flutter_blog/lib/pages/post/home_page.dart
+++ flutter_blog/lib/pages/post/home_page.dart
@@ -7,8 +7,18 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       drawer: _navigation(context),
       appBar: AppBar(),
-      body: Center(
-        child: Text("Home Page"),
+      body: ListView.separated(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {},
+            title: Text("제목1"),
+            leading: Text("1"),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
       ),
     );
   }
