d2678700e033ee939d7d2f3f992c038badc40bdf
unknown <qwq140@gmail.com>
2021-08-31 14:45:39 +0900

    2. 블로그 - 회원가입 페이지

--- flutter_blog/lib/pages/user/join_page.dart
+++ flutter_blog/lib/pages/user/join_page.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/components/custom_elevated_button.dart';
+import 'package:flutter_blog/components/custom_text_form_field.dart';
 
 class JoinPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 200,
+              child: Text(
+                "회원가입 페이지",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            _joinForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _joinForm() {
+    return Form(
+      child: Column(
+        children: [
+          CustomTextFormField(hint: "Enter username"),
+          CustomTextFormField(hint: "Enter password"),
+          CustomTextFormField(hint: "Enter email"),
+          CustomElevatedButton(text: "회원가입"),
+        ],
+      ),
+    );
   }
 }
